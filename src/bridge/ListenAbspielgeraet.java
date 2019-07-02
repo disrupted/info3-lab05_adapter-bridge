606733406c23a83d5aef9decb012b5ea2141de36
disrupted <hi@salomonpopp.me>
2019-07-02 19:56:27 +0200

    Playlist

--- src/bridge/ListenAbspielgeraet.java
+++ src/bridge/ListenAbspielgeraet.java
@@ -1,8 +1,22 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ListenAbspielgeraet extends Abspielgeraet {
+
+  private ArrayList<String> tracks;
 
   public ListenAbspielgeraet(IAbspielgeraetImplementierer abspielgeraet) {
     super(abspielgeraet);
+  }
+
+  public void play(AbspielDaten daten) {
+    abspielgeraet.readData(daten);
+    tracks = new ArrayList<>(Arrays.asList(daten.toString().split(";")));
+    for (int trackId = 1; trackId <= tracks.size(); trackId++) {  // play tracks one after another in chronological order
+      abspielgeraet.skipTo(trackId);
+      abspielgeraet.play();
+    }
   }
 }
